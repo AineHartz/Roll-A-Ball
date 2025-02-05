e6f7022f32535b2641f7ce4e3b5a1684ec4f30dc
Ryan <ryanhartz9169@gmail.com>
2025-02-05 14:01:25 -0600

    Updated ball spawning
    Balls no longer fall straight down upon being spawned: A slight, random horizontal force is applied, to make sure clicking the same spot doesn't lead to the exact same result. This is based on gambling, too much control is bad!

--- Assets/Scripts/Pachinko Scripts/SpawnSpace Script.cs
+++ Assets/Scripts/Pachinko Scripts/SpawnSpace Script.cs
@@ -5,8 +5,28 @@ public class SpawnSpaceScript : MonoBehaviour, Clickable
     //Says which ball to spawn
     public GameObject ballPrefab;
 
+    //How fast balls are launched downwards on spawn
+    float downwardForce = 2f;
+
     public void onClick(Vector3 hitPoint)
     {
-        Instantiate(ballPrefab, hitPoint, Quaternion.identity);
+        GameObject newBall = Instantiate(ballPrefab, hitPoint, Quaternion.identity);
+
+        
+        /*
+         * The below lines of code handle the "bloom" effect. Meaning, when a ball is spawned, it doesn't go straight down, it spreads out a bit,
+         * so every ball spawned at the same spot doesn't follow the same path. The rigidbody of the new ball is fetched, a small random number is generated,
+         * and then a force vector is created that will make sure the ball always goes downward, but with a slight variation either way. Then, the forcevector is
+         * applied to the ball's rigidbody. 
+         */
+        
+        Rigidbody rb = newBall.GetComponent<Rigidbody>();
+        float randomX = Random.Range(-2f, 2f);
+        
+        //The normalization makes sure that the downward force applied is consistent. 
+        Vector3 forceVector = new Vector3(randomX, -1f, 0f).normalized * downwardForce;
+
+        // VelocityChange makes the added force apply immediately. 
+        rb.AddForce(forceVector, ForceMode.VelocityChange);
     }
 }
