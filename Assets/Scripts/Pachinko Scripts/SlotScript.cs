fb4da6ecc4589d5cad6cc38a78afaeb5d5f43889
Ryan <ryanhartz9169@gmail.com>
2025-02-05 13:31:08 -0600

    Updated SlotScript
    Bottom slot script functions as intended for now. Each pair of slots has it's own base point value, and deletes the ball after passing the point value along. Right now, that's just to a debug log.

--- Assets/Scripts/Pachinko Scripts/SlotScript.cs
+++ Assets/Scripts/Pachinko Scripts/SlotScript.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class SlotScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Set in inspector. This is the base point value for each slot, before any multipliers are applied. 
+    public int basePoints;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.CompareTag("Ball"))
+        {
+            Debug.Log("Detected ball! Points for this slot: " + basePoints);
+            Destroy(other.gameObject);
+        }
     }
 }
