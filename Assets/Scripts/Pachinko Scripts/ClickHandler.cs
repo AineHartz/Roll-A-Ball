68fd5a06d01aaa4877da9050aec4e9186275647a
Ryan <ryanhartz9169@gmail.com>
2025-02-05 11:10:44 -0600

    Added click manager!
    If you click an area in the clickable layer, a ball is now spawned! Ball spawn needs to be moved from the click manager to the script within the spawn space itself, but it works for now.

---
+++ Assets/Scripts/Pachinko Scripts/ClickHandler.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+//I decided to go with this method of handling clicks with help from Chatgpt. Especially when I get to making upgrades, there'll be a good
+//few different clickable areas, so it's best to make one class that handles clicking instead of writing the same code into the update method of half a dozen scripts. 
+
+public class ClickHandler : MonoBehaviour
+{
+    //Says which ball to spawn
+    public GameObject ballPrefab;
+
+    //Every clickable area will be set to the clickable layer, to make sure the click collider doesn't mess with the game in any way.
+    public LayerMask clickable;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Casts a ray from camera to where you clicked
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            /*
+             * Ray is the ray created by the click. Out hit is the output of the RaycastHit, storing information about the intersected collider.
+             * Mathf.Infinity means the ray will keep going either forever or until it hits something. Clickable makes sure only something in the clickable
+             * layer will be acted upon.
+             */
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
+            {
+                //Spawns the right type of ball at the point where you clicked, with no rotation. 
+                Instantiate(ballPrefab, hit.point, Quaternion.identity);
+            }
+
+
+        }
+    }
+}
