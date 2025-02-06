a05d075cb74b4771f343cea5b266a2b408594dc0
Ryan <ryanhartz9169@gmail.com>
2025-02-06 10:42:22 -0600

    fixed ball getting stuck (again)
    Setting friction to 0 helped alot, but the ball getting stuck still happened. I made the pin colliders into diamonds into spheres so less of a balance point, which helped, but it still happened. So, added a script to the ball to nudge it to either side if it sits still for too long. I think this will actually fix it completely.

---
+++ Assets/Scripts/Pachinko Scripts/BallScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float timer = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        /*
+         * The below script is to help the ball self-correct if it gets stuck. If the ball's speed drops too low for too long, a small horizontal
+         * force is applied to get it moving again. 
+         */
+        if (rb.linearVelocity.magnitude < 0.1f)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 1.5f)
+            {
+                Vector3 nudgeDirection = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+                rb.AddForce(nudgeDirection * 2f, ForceMode.Impulse);
+                timer = 0f;
+            }
+        }
+
+        else
+        {
+            timer = 0f;
+        }
+    }
+}
